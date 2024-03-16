316bd27cc04e0dae0e8b05f6d5431ab01f46c53e
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-03-17 01:43:26 +0600

    data loda and deynamic data show complete

--- src/Recipes/Recipes.jsx
+++ src/Recipes/Recipes.jsx
@@ -1,5 +1,8 @@
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
+import { CiClock1 } from "react-icons/ci";
+import { AiTwotoneFire } from "react-icons/ai";
+
 
 const Recipes = props => {
     const [recipe, setRecipe] = useState([]);
@@ -12,29 +15,40 @@ const Recipes = props => {
     }, [])
     return (
         <div>
-            <h1>This {recipe.length}</h1>
             <div className='grid grid-cols-2 gap-5'>
                 {
                     recipe.map(item => {
                         return (
-                            <div key={item.id}>
-                                <div className="card card-compact bg-base-100 shadow-xl">
+                            <div key={item.id} className='h-[72vh] '>
+                                <div className="card card-compact bg-base-100 shadow-xl p-5 border-2">
                                     <figure><img src={item.image} alt="Shoes" /></figure>
                                     <div className="card-body">
                                         <h2 className="card-title">{item.name}</h2>
                                         <p>{item.description}</p>
                                         <p>Ingredients : {item.ingredients.length}</p>
-                                        {
-                                            item.ingredients.slice(0, 3).map((pd, index) => {
-                                                return (
-                                                    <div key={index}>
-                                                        <p>{pd.ingredient_name}, {pd.quantity}</p>
-                                                    </div>
-                                                )
-                                            })
-                                        }
-                                        <div className="card-actions justify-end">
-                                            <button className="btn btn-primary">Buy Now</button>
+                                        <ul className='list-disc'>
+                                            {
+                                                item.ingredients.slice(0, 3).map((pd, index) => {
+                                                    return (
+                                                        <div key={index}>
+                                                            <li>{pd.ingredient_name}, {pd.quantity}</li>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </ul>
+                                        <div className='flex justify-between'>
+                                            <div className='flex gap-2 items-center'>
+                                                <CiClock1 className='text-2xl' />
+                                                <p>{item.preparing_time} minutes</p>
+                                            </div>
+                                            <div className='flex gap-2 items-center'>
+                                                <AiTwotoneFire  className='text-2xl'/>
+                                                <p>{item.calories}</p>
+                                            </div>
+                                        </div>
+                                        <div className="card-actions justify-start">
+                                            <button className="btn btn-primary">Want to Cook</button>
                                         </div>
                                     </div>
                                 </div>
